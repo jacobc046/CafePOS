41f74a05901d6e455de3ef88d6a71114ad1320f3
Jacob Croket <thisisjacobseml@icloud.com>
2024-08-02 18:43:06 -0400

    Initial Commit

---
+++ Cafe POS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cafe POS
+//
+//  Created by Jacob Croket on 8/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
